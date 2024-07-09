01c6d08cf0fdda470430e6aaa8e6cfd9cc628c89
Rusnakov <tomasz.rusnak@gmail.com>
2024-07-09 10:55:27 +0200

    feat: adding the mechanics of the drawText function

--- js/script.js
+++ js/script.js
@@ -5,7 +5,12 @@ let canvas, ctx;
 
 let x = Math.floor(CANVAS_WIDTH / 2);
 let y = Math.floor(CANVAS_HEIGHT / 2);
-let num = 1;
+
+let step = 1;
+let stepSize = 50;
+let numSteps = 1;
+let state = 0;
+let turnCounter = 1;
 
 const cols = Math.floor(CANVAS_WIDTH);
 const rows = Math.floor(CANVAS_HEIGHT);
@@ -21,15 +26,56 @@ function resizeCanvas() {
   canvas.width = CANVAS_WIDTH;
 }
 
-function drawText(text, x, y) {
-  ctx.font = "64px Arial";
+function setupText() {
+  ctx.font = "20px Arial";
   ctx.fillStyle = "clack";
   ctx.textAlign = "center";
   ctx.textBaseline = "middle";
-  ctx.fillText(text, x, y);
 }
 
-window.addEventListener('load', () => {
+function drawText() {
+  for (let i = 1; i <= 121; i++) {
+    ctx.fillText(step, x, y);
+
+    switch (state) {
+      case 0: {
+        x += stepSize;
+        break;
+      }
+      case 1: {
+        y -= stepSize;
+        break;
+      }
+      case 2: {
+        x -= stepSize;
+        break;
+      }
+      case 3: {
+        y += stepSize;
+        break;
+      }
+    }
+    
+    if (step % numSteps === 0) {
+      state = (state + 1) % 4;
+      turnCounter++;
+      if (turnCounter % 2 === 0) {
+        numSteps++;
+      }
+    }
+    step++;
+
+    console.log(
+      step,
+      "numSteps = " + numSteps,
+      "tC = " + turnCounter,
+      "state = " + state
+    );
+  }
+}
+
+window.addEventListener("load", () => {
   initializeCanvas();
-  drawText('1',x,y);
+  setupText();
+  drawText();
 });
